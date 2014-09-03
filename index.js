2c233c0e83296bb2e91d0fb445a5258961c9764f
Cheng Xing <xc561@hotmail.com>
2014-09-02 23:31:21 -0400

    initial setup for socket.io

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var app = require("express")();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
 
